1ff714a6370556ee0a3c12a36d207b03ba89656e
SCljh <763986907@qq.com>
2020-07-25 14:34:26 +0800

    完善“面向用户添加任务接口”，添加cpp版本及测试用例

--- main.cpp
+++ main.cpp
@@ -2,17 +2,23 @@
 // Created by acmery on 2020/5/27.
 //
 
+#define CPP_TEST
+
+#include <stdio.h>
+
+#ifndef CPP_TEST
+
 #include "threadpool.h"
-#include <iostream>
 
 void testFun(void* arg){
     printf("i = %d\n", *(int *)arg);
+    //注意：std::cout非线程安全
     //std::cout << "i = " << *(int *)arg << std::endl;
 }
 
 int main(){
     nThreadPool *pool = (nThreadPool*)malloc(sizeof(nThreadPool));
-    int ret = nThreadPoolCreate(pool, 1);
+    int ret = nThreadPoolCreate(pool, 1000);
     if (ret != 1){
         perror("线程池初始化失败");
         printf("ret = %d\n", ret);
@@ -26,3 +32,24 @@ int main(){
 
     nThreadPoolDestroy(pool);
 }
+
+#else
+
+#include "threadpool.hpp"
+
+void testFun(void* arg){
+    printf("i = %d\n", *(int *)arg);
+}
+
+
+int main(){
+    ThreadPool *pool = new ThreadPool(1000, 2000);
+
+    printf("线程池初始化成功\n");
+    int i = 0;
+    for (i = 0; i < 1000; ++i) {
+        pool->pushJob(testFun, &i, sizeof(int));
+    }
+}
+
+#endif
